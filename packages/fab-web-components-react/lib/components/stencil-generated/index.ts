ffa6b5501c65308fe3e381164ac93a69411f5ce1
fabienpuissant <fabienpuissant@live.fr>
2024-02-23 14:50:23 +0100

    rebuild

--- packages/fab-web-components-react/lib/components/stencil-generated/index.ts
+++ packages/fab-web-components-react/lib/components/stencil-generated/index.ts
@@ -5,6 +5,7 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from 'fab-web-components';
 
-export { defineCustomElements } from "fab-web-components/loader";
+import { defineCustomElements } from 'fab-web-components/loader';
 
+defineCustomElements();
 export const FabButton = /*@__PURE__*/createReactComponent<JSX.FabButton, HTMLFabButtonElement>('fab-button');
--- packages/my-app/src/App.tsx
+++ packages/my-app/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { FabButton } from 'fab-web-components-react';
 
-
 function App() {
   const onClick = () => {
     console.log("ok");
